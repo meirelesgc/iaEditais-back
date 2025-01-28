701d2cb030ae2343ca75654387e06fe5586f4ed4
meirelesgc <geu_costa@outlook.com>
2025-01-27 23:49:49 -0300

    Organizando os arquivos

--- iaEditais/__init__.py
+++ iaEditais/__init__.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from iaEditais.config import Settings
 from iaEditais.routers.SourceRouter import router as source_router
-from iaEditais.routers.TaxonomyRouter import router as taxonomy_router
+from iaEditais.routers.Taxonomy.GuidelinesRouter import (
+    router as guidelines_router,
+)
+from iaEditais.routers.Taxonomy.EvaluationsRouter import (
+    router as evaluations_router,
+)
+
 
 app = FastAPI(root_path=Settings().ROOT_PATH)
 
 app.include_router(source_router, tags=['Source'])
-app.include_router(taxonomy_router, tags=['Taxonomy'])
+app.include_router(guidelines_router, tags=['Taxonomy', 'Guidelines'])
+app.include_router(evaluations_router, tags=['Taxonomy', 'Evaluations'])
 
 
 @app.get('/')
--- iaEditais/routers/Taxonomy/EvaluationsRouter.py
+++ iaEditais/routers/Taxonomy/EvaluationsRouter.py
@@ -1,33 +1,9 @@
 from fastapi import APIRouter
 from uuid import UUID
-from iaEditais.schemas.Guideline import CreateGuideline, Guideline
 from iaEditais.schemas.Evaluation import CreateEvaluation, Evaluation
 from iaEditais.services import TaxonomyService
 
 router = APIRouter()
-
-
-@router.post('/taxonomy/guidelines/')
-def post_guideline(guideline: CreateGuideline) -> Guideline:
-    return TaxonomyService.post_guideline(guideline)
-
-
-@router.get(
-    '/taxonomy/guidelines/{guideline_id}/',
-    response_model=Guideline | list[Guideline],
-)
-def get_guideline(guideline_id: UUID = None):
-    return TaxonomyService.get_guidelines(guideline_id)
-
-
-@router.put('/taxonomy/guidelines/')
-def put_guideline(guideline: Guideline) -> Guideline:
-    return TaxonomyService.put_guideline(guideline)
-
-
-@router.delete('/taxonomy/guidelines/{guideline_id}/')
-def delete_guideline(guideline_id: UUID):
-    return TaxonomyService.delete_guideline(guideline_id)
 
 
 @router.post('/taxonomy/evaluations/')
