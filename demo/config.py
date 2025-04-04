b14f5f5d5f88c4a1fa17237bbc29e66c6715e260
gmcosta <geu_costa@outlook.com>
2025-04-04 11:08:00 -0300

    feat: migrando a demo para outro repositorio

--- demo/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    API: str = None
-
-    class Config:
-        env_file = '.demo.env'
-        env_file_encoding = 'utf-8'
