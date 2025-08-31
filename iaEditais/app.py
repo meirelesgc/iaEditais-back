fc2d6bb45fd6708824a543f562480aadf6d6dabb
gmcosta <geu_costa@outlook.com>
2025-08-31 00:01:03 -0300

    wip

---
+++ iaEditais/app.py
@@ -0,0 +1,32 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+BASE_DIR = os.path.dirname(__file__)
+STORAGE_DIR = os.path.join(BASE_DIR, 'storage')
+os.makedirs(STORAGE_DIR, exist_ok=True)
+
+UPLOADS_DIR = os.path.join(STORAGE_DIR, 'uploads')
+os.makedirs(UPLOADS_DIR, exist_ok=True)
+
+TEMP_DIR = os.path.join(STORAGE_DIR, 'temp')
+os.makedirs(TEMP_DIR, exist_ok=True)
+
+app = FastAPI()
+
+app.mount('/uploads', StaticFiles(directory=UPLOADS_DIR), name='uploads')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+@app.get('/')
+def root():
+    return {'mensagem': 'API em funcionamento!'}
---
+++ tests/conftest.py
@@ -0,0 +1,75 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+import pytest
+import pytest_asyncio
+from fastapi.testclient import TestClient
+from sqlalchemy import event
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from testcontainers.postgres import PostgresContainer
+
+from iaEditais.app import app
+from iaEditais.database import get_session
+from iaEditais.models import table_registry
+
+
+@pytest.fixture
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
+
+
+@pytest.fixture(scope='session')
+def engine():
+    with PostgresContainer('postgres:16', driver='psycopg') as postgres:
+        _engine = create_async_engine(postgres.get_connection_url())
+        yield _engine
+
+
+@pytest_asyncio.fixture
+async def session(engine):
+    async with engine.begin() as conn:
+        await conn.run_sync(table_registry.metadata.create_all)
+
+    async with AsyncSession(engine, expire_on_commit=False) as session:
+        yield session
+
+    async with engine.begin() as conn:
+        await conn.run_sync(table_registry.metadata.drop_all)
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
+    def fake_insert_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    def fake_update_time_hook(mapper, connection, target):
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    def fake_delete_time_hook(mapper, connection, target):
+        if hasattr(target, 'deleted_at'):
+            target.deleted_at = time
+
+    event.listen(model, 'before_insert', fake_insert_time_hook)
+    event.listen(model, 'before_update', fake_update_time_hook)
+    event.listen(model, 'before_update', fake_delete_time_hook)
+
+    yield time
+
+    event.remove(model, 'before_insert', fake_insert_time_hook)
+    event.remove(model, 'before_update', fake_update_time_hook)
+
+
+@pytest.fixture
+def mock_db_time():
+    return _mock_db_time
