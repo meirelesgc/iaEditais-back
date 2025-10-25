db632eac8857caf028b2b78c0f3f206382776ace
gmcosta <geu_costa@outlook.com>
2025-10-25 06:58:37 -0300

    wip

--- iaEditais/core/database.py
+++ iaEditais/core/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import desc, event
+from sqlalchemy import event
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
@@ -33,25 +33,6 @@ def _add_filtering_criteria(execute_state: ORMExecuteState):
                 include_aliases=True,
             )
         )
-
-
-@event.listens_for(Session, 'do_orm_execute')
-def _add_default_ordering(execute_state: ORMExecuteState):
-    skip_ordering = execute_state.execution_options.get(
-        'skip_default_ordering', False
-    )
-
-    if execute_state.is_select and not skip_ordering:
-        for entity in execute_state.statement._raw_columns:
-            if (
-                hasattr(entity, 'entity_zero')
-                and entity.entity_zero is not None
-            ):
-                cls = entity.entity_zero.class_
-                if hasattr(cls, 'created_at'):
-                    execute_state.statement = execute_state.statement.order_by(
-                        desc(cls.created_at)
-                    )
 
 
 async def get_session():
