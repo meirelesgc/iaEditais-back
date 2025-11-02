2889d2d37149bdead778664f5f9607275c2d7899
gmcosta <geu_costa@outlook.com>
2025-11-02 11:51:50 -0300

    ajuste no .env do alembic

--- migrations/env.py
+++ migrations/env.py
@@ -41,7 +41,11 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection):
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        include_object=include_object
+    )
 
     with context.begin_transaction():
         context.run_migrations()
